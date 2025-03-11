e07871d09925b576dbd0e903a5a4cb54cd1550a5
FAlmeida-switch <1241906@isep.ipp.pt>
2025-03-11 15:29:34 +0000

    Added EmployeeTest class.

--- CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
+++ CA1/part1/basic/src/test/java/com/greglturnquist/payroll/EmployeeTest.java
@@ -3,12 +3,11 @@ package com.greglturnquist.payroll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeTest {
 
 
